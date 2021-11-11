a3dccb2ebedc3cd9477568e104af103acb0a1333
Bartosz Pikulinski <bartosz.pikulinski@gmail.com>
2021-11-11 12:09:16 +0100

    Mainpage dynamic counting of institutions and bags, clarified model str representation

--- charity_donation/models.py
+++ charity_donation/models.py
@@ -1,12 +1,19 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
 class Category(models.Model):
+    def __str__(self):
+        return self.name
+
     name = models.CharField(max_length=128, null=True, default="Brak kategorii")
 
 
 class Institution(models.Model):
+    def __str__(self):
+        return self.name
+
     CHOICES = (
         ('fundacja', 'fundacja'),
         ('organizacja pozarządowa', 'organizacja pozarządowa'),
