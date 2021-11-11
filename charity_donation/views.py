26b3f87c73154e94057173d3ae35b57dc1884197
Bartosz Pikulinski <bartosz.pikulinski@gmail.com>
2021-11-11 11:28:07 +0100

    Changed templates and added basic views

--- charity_donation/views.py
+++ charity_donation/views.py
@@ -7,3 +7,15 @@ from django.views import View
 class LandingPage(View):
     def get(self, request):
         return render(request, 'charity_donation/index.html')
+
+class AddDonation(View):
+    def get(self, request):
+        return render(request, 'charity_donation/form.html')
+
+class Login(View):
+    def get(self, request):
+        return render(request, 'charity_donation/login.html')
+
+class Register(View):
+    def get(self, request):
+        return render(request, 'charity_donation/register.html')
