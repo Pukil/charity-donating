3b432580a479ea74411c13d08903fc4d72f10021
Bartosz Pikulinski <bartosz.pikulinski@gmail.com>
2021-11-11 11:52:23 +0100

    Donation model + migration

--- charity_donation/models.py
+++ charity_donation/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -15,3 +16,17 @@ class Institution(models.Model):
     description = models.TextField()
     categories = models.ManyToManyField(Category)
     type = models.CharField(max_length=128, choices=CHOICES, default=CHOICES[0])
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.TextField()
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
