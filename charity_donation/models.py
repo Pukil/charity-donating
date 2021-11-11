1a2990d37910f3b662893ca9ebbc8f8e080a3fe7
Bartosz Pikulinski <bartosz.pikulinski@gmail.com>
2021-11-11 11:44:15 +0100

    Settings changes and Institution model

--- charity_donation/models.py
+++ charity_donation/models.py
@@ -5,4 +5,13 @@ class Category(models.Model):
     name = models.CharField(max_length=128, null=True, default="Brak kategorii")
 
 
-
+class Institution(models.Model):
+    CHOICES = (
+        ('fundacja', 'fundacja'),
+        ('organizacja pozarządowa', 'organizacja pozarządowa'),
+        ('zbiórka lokalna', 'zbiórka lokalna'),
+    )
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    categories = models.ManyToManyField(Category)
+    type = models.CharField(max_length=128, choices=CHOICES, default=CHOICES[0])
